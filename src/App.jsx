991fcb35d3f2b50474099d07177e8c686490648f
B-Selin <selin@bozkurt.ca>
2023-08-09 17:58:36 -0600

    lab part 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import SkillList from "./SkillList";
 import NewSkillForm from "./NewSkillForm";
 import './App.css'
 import './NewSkillForm.css'
 import './SkillListItem.css'
 
-const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3 },
-  { name: "JavaScript", level: 4 },
-  { name: "Python", level: 2 },
-];
+
 
 export default function App() {
+  const [skills, setSkills] = useState([
+    { name: "HTML", level: 5 },
+    { name: "CSS", level: 3 },
+    { name: "JavaScript", level: 4 },
+    { name: "Python", level: 2 }
+  ]);
+
   return (
     <div className="App">
       <h1>React Dev Skills</h1>
