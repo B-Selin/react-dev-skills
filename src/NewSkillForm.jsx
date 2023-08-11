da3c4a5fa4c2028b2c1f6059d38d8129d748e004
B-Selin <selin@bozkurt.ca>
2023-08-10 18:55:06 -0600

    lab part 4

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,18 +1,56 @@
-export default function NewSkillForm() {
-  return (
-    <form action="" className="NewSkillForm">
-      <label htmlFor="input"> Skill </label>
-      <input type="text" id="input" />
-      <label htmlFor="select">Level{" "} </label>
-      <select id="select">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
+import { useState } from "react";
 
-      <button id="button">ADD SKILL</button>
-    </form>
+export default function NewSkillForm({ addSkill }) {
+  // creating a variable to assign default values for new skill from
+  const initialSkill = {
+    name: '',
+    level: 3
+  }
+
+  // using the useState hook from React to initialize and manage state for a variable called newSkill. 
+  //  we are using array destructuring to extract those two values into separate variables:
+  // newSkill - This will hold the current value of the state 
+  // setNewSkill - This is a function that can be used to update the state value
+  const [newSkill, setNewSkill] = useState(initialSkill);
+
+
+  // creating the handleAddSkill function
+  function handleAddSkill(evt) {
+    evt.preventDefault();
+    addSkill(newSkill);
+    setNewSkill(initialSkill);
+  }
+
+  function handleChange(evt) {
+    const newSkillData = { ...newSkill, [evt.target.name]: evt.target.value };
+    setNewSkill(newSkillData)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleAddSkill} className="NewSkillForm">
+        <label> Skill </label>
+        <input
+          value={newSkill.name}
+          onChange={handleChange}
+          name="name"
+          type="text"
+        />
+        <label>Level</label>
+        <select
+          value={newSkill.level}
+          onChange={handleChange}
+          name="level"
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+
+        <button type="submit" id="button">ADD SKILL</button>
+      </form>
+    </>
   );
 }
